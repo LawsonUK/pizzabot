681e9004acd59773feff986f9c581d20a2a4ffa4
LawsonUK <chrislawsonuk@hotmail.com>
2020-11-05 15:45:42 +0000

    adding validation tests to pizzabot class

--- tests/classes/pizzabot.test.js
+++ tests/classes/pizzabot.test.js
@@ -6,17 +6,34 @@ beforeAll(() => {
   args = '5x5 (0, 0) (1, 3) (4, 4) (4, 2) (4, 2) (0, 1) (3, 2) (2, 3) (4, 1)'
 })
 
-test(`should test to see PizzaBot's decipherInstructions method throw an error if no instructions are provided`, () => {
+/**
+ * Setup Tests
+ */
+
+test(`should setup the PizzaBot object with argument instructions`, () => {
+  const pizzabot = new PizzaBot(args)
+  expect(pizzabot.Grid.size).toBe('5x5')
+  // expect(pizzabot.Grid.houses).toBe(
+  //   '(0,0)(1,3)(4,4)(4,2)(4,2)(0,1)(3,2)(2,3)(4,1)'
+  // )
+  expect(pizzabot.currentLocation).toEqual([0, 0])
+  expect(pizzabot.outputLogInstructions).toBe('')
+})
+
+/**
+ * Validation Tests
+ */
+
+test(`should test to see PizzaBot's decipherInstructions method throws an error if no instructions are provided`, () => {
   expect(() => {
     new PizzaBot()
   }).toThrowError(/not provided any instructions/)
 })
 
-test(`should setup the PizzaBot object with argument instructions`, () => {
-  const pizzabot = new PizzaBot(args)
-  expect(pizzabot.deliveryInstructions).toBe(
-    '5x5 (0, 0) (1, 3) (4, 4) (4, 2) (4, 2) (0, 1) (3, 2) (2, 3) (4, 1)'
-  )
-  expect(pizzabot.currentLocation).toEqual([0, 0])
-  expect(pizzabot.outputLogInstructions).toBe('')
+test(`should test to see PizzaBot's decipherInstructions method throws an error if grid size is not formatted correctly`, () => {
+  const test_args =
+    '55 (0, 0) (1, 3) (4, 4) (4, 2) (4, 2) (0, 1) (3, 2) (2, 3) (4, 1)'
+  expect(() => {
+    new PizzaBot(test_args)
+  }).toThrowError(/you have not provided a grid size/)
 })
